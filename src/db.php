d2a991c5af8ea6a95dfce78dcccfb04a76a480f8
abdulhabibalkithri <ahmedalkithri@gmail.com>
2025-05-11 15:48:40 +0300

    db.php

--- src/db.php
+++ src/db.php
@@ -1,9 +1,10 @@
 <?php
-$host = getenv('DB_HOST');  // Database host
+$host = getenv('DB_HOST');  // Database host, hapa ni 'db' au jina la container yako
 $user = getenv('DB_USER');  // Database username
 $password = getenv('DB_PASSWORD');  // Database password
 $database = getenv('DB_NAME');  // Database name
 
+// Kujaribu kuungana na database
 $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
