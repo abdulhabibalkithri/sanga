ae45301c0bdea7b7ea333f464e8b7665f49cc9d2
abdulhabibalkithri <ahmedalkithri@gmail.com>
2025-05-11 15:57:04 +0300

    db.php

--- src/db.php
+++ src/db.php
@@ -1,12 +1,22 @@
 <?php
-$host = getenv('DB_HOST');  // Database host, hapa ni 'db' au jina la container yako
+// Confirming if environment variables are being set correctly
+$host = getenv('DB_HOST');  // Database host
 $user = getenv('DB_USER');  // Database username
 $password = getenv('DB_PASSWORD');  // Database password
 $database = getenv('DB_NAME');  // Database name
 
+// Check if environment variables are null or empty
+if (!$host || !$user || !$password || !$database) {
+    die("Environment variables are not set properly.");
+}
+
 // Kujaribu kuungana na database
 $conn = new mysqli($host, $user, $password, $database);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully!";
 ?>
