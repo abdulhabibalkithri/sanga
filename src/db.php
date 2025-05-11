f263958179b98e0c92649950110668e4dbed6b1c
abdulhabibalkithri <ahmedalkithri@gmail.com>
2025-05-11 15:39:29 +0300

    db.php

--- src/db.php
+++ src/db.php
@@ -1,11 +1,10 @@
 <?php
-$host = 'db'; // container name from docker-compose
-$user = 'sanga_user';
-$password = 'sanga_pass';
-$database = 'chat';
+$host = getenv('DB_HOST');  // Database host
+$user = getenv('DB_USER');  // Database username
+$password = getenv('DB_PASSWORD');  // Database password
+$database = getenv('DB_NAME');  // Database name
 
 $conn = new mysqli($host, $user, $password, $database);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
